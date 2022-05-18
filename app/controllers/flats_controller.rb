2b724516f5c9e3dc457d8748aa8ac315121a5ce5
valsmets <valsmets@hotmail.com>
2022-05-18 15:30:06 +0200

    rails new

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,35 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @flats = Flat.all
+  end
+  def show
+  end
+  def new
+    @flat = Flat.new
+  end
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+  def edit
+  end
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
+  def destroy
+    @flat.destroy
+    # no need for app/views/flats/destroy.html.erb
+    redirect_to flats_path
+  end
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :details, :completed)
+  end
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
